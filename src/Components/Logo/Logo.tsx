363b4885f1e36db0cd50b32600f3620dc88ec2b5
Rafi Ali <therafiali@gmail.com>
2024-05-24 12:59:10 +0500

    navbar

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,28 +1,26 @@
 import Image from "next/image";
-import logo from "@/assets/rafiali.png";
+import logo from "@/assets/therafiali.svg";
 import logo1 from "@/assets/giphy.gif";
 import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center cursor-pointer border border-red-500">
-      <Link href={'/'}>
-      
+    <div className="flex justify-center items-center cursor-pointer h-16">
+      <Link href={"/"}>
         <Image
           className=" select-none rotate-[-60deg] scale-x-[-1] "
           src={logo1}
-          width={200}
+          width={100}
           height={100}
           alt="Rafi Ali"
         />
       </Link>
-      <Link href={'/'}>
-      
+      <Link href={"/"}>
         <Image
           className=" select-none "
           src={logo}
-          width={200}
-          height={100}
+          width={150}
+          height={150}
           alt="Rafi Ali"
         />
       </Link>
