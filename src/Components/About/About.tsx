2448a15ede3a940d4ee1e1559d2bf6fd7c31456a
Rafi Ali <therafiali@gmail.com>
2024-06-03 16:21:52 +0500

    add about me

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>I'm a passionate developer who thrives on building smart solutions across the digital landscape. Websites, tools, scripts, APIs, and even generative AI-powered chatbots—I leverage cloud-native technologies to create innovative platforms that adapt and grow. Let's collaborate on your next digital adventure.</div>
+  )
+}
+
+export default About
