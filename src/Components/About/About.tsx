5ab0aaf6c9509b9f1f815ae40fc597fa454fba2f
Rafi Ali <therafiali@gmail.com>
2024-06-04 18:49:50 +0500

    add about me

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Greenred from "@/assets/greenred.svg";
+import Yellow from "@/assets/Yellow.png";
+import Image from "next/image";
 
 const About = () => {
   return (
-    <div>I'm a passionate developer who thrives on building smart solutions across the digital landscape. Websites, tools, scripts, APIs, and even generative AI-powered chatbots—I leverage cloud-native technologies to create innovative platforms that adapt and grow. Let's collaborate on your next digital adventure.</div>
-  )
-}
+    <div className="pt-20 relative overflow-hidden">
+      <Image className="absolute animate-bounce" src={Greenred} alt="" />
+      <Image className="absolute right-0 animate-bounce duration-75" src={Yellow} alt="" height={150} width={150} />
+      <h2 className="flex  flex-col max-w-screen-md from-accent-foreground mx-auto scroll-m-20 text-2xl text-justify font-medium tracking-widest lg:text-2xl mt-14 mb-28 leading-relaxed">
+        <p>
+          I'm a passionate developer who thrives on building smart solutions
+          across the digital landscape. Websites, tools, scripts, APIs, and even
+          generative AI-powered chatbots—I leverage cloud-native technologies to
+          create innovative platforms that adapt and grow. Let's collaborate on
+          your next digital adventure.
+        </p>
+      </h2>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
- import Hero from "@/Components/Hero/Hero";
- import NavbarHeader from "@/Components/Navbar/Navbar";
- import Slider from "@/Components/Slider/Slider";
- import SliderSection from "@/Components/Slider/SliderSection";
+import About from "@/Components/About/About";
+import Hero from "@/Components/Hero/Hero";
+import NavbarHeader from "@/Components/Navbar/Navbar";
+import Slider from "@/Components/Slider/Slider";
+import SliderSection from "@/Components/Slider/SliderSection";
 
- const Home = () => {
-   return (
-     <main className="bg-image">
-       <Hero />
-       <SliderSection />
-       <Hero />
-       {/* <Hero /> */}
-       <NavbarHeader />
-     </main>
-   );
- };
+const Home = () => {
+  return (
+    <main className="bg-image">
+      <Hero />
+      <SliderSection />
+      <About />
+      <NavbarHeader />
+    </main>
+  );
+};
 
- export default Home;
+export default Home;
