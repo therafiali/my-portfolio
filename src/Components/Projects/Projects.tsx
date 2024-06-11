01e7c92e8a4268ebf265c37902bc0aaa4cce9f9d
Rafi Ali <therafiali@gmail.com>
2024-06-11 12:12:24 +0500

    projects for mobile

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -17,8 +17,8 @@ import Link from "next/link";
 const Projects = () => {
   return (
     <div>
-      <section id="portfolio" className="pt-20 relative overflow-hidden">
-        <Image
+      <section id="portfolio" className="sm:pt-20 relative overflow-hidden">
+        {/* <Image
           className="absolute myDuration animate-bounce"
           src={Greenred}
           alt=""
@@ -27,23 +27,23 @@ const Projects = () => {
           className="absolute right-0 myDuration animate-bounce"
           src={Yellow}
           alt=""
-       
-        />
-        <h2 className="flex  flex-col max-w-screen-md from-accent-foreground mx-auto scroll-m-20 text-2xl text-justify font-medium tracking-widest lg:text-2xl mt-14 mb-28 leading-relaxed">
-          <small className="text-sm font-medium leading-none text-center">
+        /> */}
+         <h2 className="scroll-m-20  sm:text-4xl font-extrabold tracking-tight lg:text-5xl text-center ">Projects</h2>
+        <h2 className="flex  flex-col max-w-screen-md from-accent-foreground mx-auto scroll-m-20 text-2xl text-justify font-medium tracking-widest lg:text-2xl sm:mt-14  sm:mb-28 leading-relaxed">
+          <small className="mt-2 sm:mt-0 px-2 sm:px-0 font-sans text-sm font-medium leading-none text-center">
             A portfolio is more than a collection of projects,
           </small>
-          <div className="mx-auto flex flex-col items-center">
-            <blockquote className="mt-6 border-l-2 pl-6 italic ">
+          <div className="mx-auto mt-2 sm:mt-0 flex flex-col items-center text-lg ">
+            <blockquote className="sm:mt-6 border-l-2 sm:pl-6 italic text-center">
               “ it&apos;s a showcase of progress, what you&apos;ve learned,
             </blockquote>
-            <blockquote className="mt-6 border-l-2 pl-6 italic ">
+            <blockquote className="sm:mt-6 border-l-2 sm:pl-6 italic text-center">
               and your plans for what comes next.”
             </blockquote>
           </div>
         </h2>
       </section>
-      <div className="grid grid-cols-2  ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 px-2 sm:px-0 ">
         <div className="col-span-1 ">
           <Link
             target="_blank"
