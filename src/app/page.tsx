440f44e0e2f811d03c28fbba02b5673caa8a494a
Rafi Ali <therafiali@gmail.com>
2024-06-11 11:55:21 +0500

    for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from "@/Components/Contact/Form";
 import Footer from "@/Components/Footer/Footer";
 import Hero from "@/Components/Hero/Hero";
 import NavbarHeader from "@/Components/Navbar/Navbar";
-import Projects from "@/Components/Porjects/Projects";
+import Projects from "@/Components/Projects/Projects";
 import Skills from "@/Components/Skills/Skills";
 import Skillstree from "@/Components/Skills/Skillstree";
 import SliderSection from "@/Components/Slider/SliderSection";
