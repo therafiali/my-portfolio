dde266eb3365b797084229c9edfccccb84e6a4f0
Rafi Ali <therafiali@gmail.com>
2024-06-13 18:57:51 +0500

    add github links

--- src/Components/Projects/BlackCard.tsx
+++ src/Components/Projects/BlackCard.tsx
@@ -25,79 +25,96 @@ export const projects = [
     title: "Ecommerce Web App",
 
     link: "https://full-stack-ecommerce-clothing-web.vercel.app/",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: [
+      "Nextjs",
+      "React",
+      "Javasript",
+      "Typescript",
+      "Redux",
+      "Sanity",
+      "Stipe",
+      "Tailwind CSS",
+      "Drizzle ORM",
+    ],
+    code: "https://github.com/therafiali/Full_Stack_ecommerce_project-01",
   },
   {
     image: first,
     title: "Simple Portfolio Web",
 
     link: "https://first-portfolio-three.vercel.app/",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Nextjs", "React", "Javasript", "Typescript", "Tailwind CSS"],
+    code: "https://github.com/therafiali/first_portfolio",
   },
   {
     image: portfolio,
     title: "Portfolio Web",
 
     link: "https://therafiali.vercel.app/",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Nextjs", "React", "Javasript", "Typescript", "Tailwind CSS"],
+    code: "https://github.com/therafiali/my-portfolio",
   },
   {
     image: todo,
     title: "Todo App",
 
     link: "https://todo-app-one-black.vercel.app/",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: [
+      "Nextjs",
+      "React",
+      "Javasript",
+      "Typescript",
+      "Tailwind CSS",
+      "PostgreSQL",
+    ],
+    code: "https://github.com/therafiali/Todo-app",
   },
   {
     image: gallery,
     title: "Image Gallery App using Cloudinary API",
 
     link: "https://image-gallery-app-gamma.vercel.app/gallery",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Nextjs", "React", "Javasript", "Typescript", "Tailwind CSS"],
+    code: "https://github.com/therafiali/images_gallery_project",
   },
   {
     image: panaverse,
     title: "Panaverse Simple web app",
 
     link: "https://panaverse-clone-theta.vercel.app/",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Nextjs", "React", "Javasript", "Typescript", "Tailwind CSS"],
+    code: "https://github.com/therafiali/Panaverse-web-app",
   },
   {
     image: chatbot,
     title: "OpenAI Based BookStore Chatbot",
 
     link: "http://bit.ly/therafialigenerativeai",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Python", "OpenAI API", "Simple Book API", "Streamlit"],
+    code: "https://github.com/therafiali/Chatbot_with_Simple_Book_Api",
   },
   {
     image: fastapi,
     title: "Crud Operations in FASTAPI - Python",
 
     link: "https://bit.ly/therafialiapi",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Python", "Fastapi", "SQl Alchemy", "Streamlit", "PostgreSQL"],
+    code: "https://github.com/therafiali/Todo_RestAPI_using_FastAPI",
   },
   {
     image: email,
     title: "Email Scraper for Email Marketing - Python Selenium",
 
     link: "https://bit.ly/therafialiapi",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Python", "Selinium"],
+    code: "https://github.com/therafiali",
   },
   {
     image: autoresponder,
     title: "Whatsapp Autoresponder for Server Monitoring - Python",
 
     link: "https://www.youtube.com/shorts/KzrXvj7wBsQ",
-    skills: ["Nextjs", "React", "Javasript", "Typescript"],
-    code: "",
+    skills: ["Python", "Selinium"],
+    code: "https://github.com/therafiali",
   },
 ];
